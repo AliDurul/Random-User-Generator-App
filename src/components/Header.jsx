9558adf0e1cb19e6623efbdf91feb46b9a7cb02e
Ali Durul <80897590+AliDurul@users.noreply.github.com>
2023-07-26 22:16:12 +0200

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cwSvg from "../assets/cw.svg";
+
+const Header = () => {
+  return (
+    <div className="block bcg-orange">
+       
+      </div>
+  )
+}
+
+export default Header
